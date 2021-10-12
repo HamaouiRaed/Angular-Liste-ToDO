995351518827eb23fec3936aad3f08e739cd1a93
RaedH <raed.hamaoui@esprit.tn>
2021-10-12 23:37:07 +0200

    use calcul-stat service

--- src/app/home-todo/home-todo.component.ts
+++ src/app/home-todo/home-todo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Todo} from '../model/todo';
+import { Todo } from '../model/todo';
 import { StatCaluculService } from '../services/stat-calucul.service';
 
 @Component({
@@ -9,26 +9,28 @@ import { StatCaluculService } from '../services/stat-calucul.service';
 })
 export class HomeTodoComponent implements OnInit {
   listTodo: Todo[];
-  StatValue!:number;
-  n!:number;
+  statValue: number = 0;
 
-  constructor(private calcul:StatCaluculService) { }
+
+  constructor(private calcul: StatCaluculService) { }
 
   ngOnInit(): void {
     this.listTodo = [
-      {userId: 12, id: 1, title: 'task 1', completed: false },
-      {userId: 12, id: 2, title: 'task 2', completed: false },
-      {userId: 12, id: 3, title: 'task 3', completed: true },
-      {userId: 12, id: 4, title: 'task 4', completed: false }
+      { userId: 12, id: 1, title: 'task 1', completed: false },
+      { userId: 12, id: 2, title: 'task 2', completed: false },
+      { userId: 12, id: 3, title: 'task 3', completed: true },
+      { userId: 12, id: 4, title: 'task 4', completed: false }
     ]
+
   }
-  changeEtat(t: Todo){
+  changeEtat(t: Todo) {
     let i = this.listTodo.indexOf(t);
     this.listTodo[i].completed = true;
   }
-  getStatService(){
-    for (let i in this.listTodo){
-      this.StatValue=this.calcul.getStat(this.listTodo, 'completed', true);
+  getStatService() {
+    for (let i in this.listTodo) {
+      this.statValue = this.calcul.getStat(this.listTodo, 'completed', true);
+      console.log(this.statValue);
 
     }
   }
